cdf7ab58fddd5102d08bd3e4345da8b076997971
yuniyolli <yuniyolli@gmail.com>
2023-04-19 21:13:46 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    public CrawlDate (LocalDateTime
+                      lastCrawlDateTime) {
+        this.lastCrawlDateTime =
+                lastCrawlDateTime;
+
+    }
+}
